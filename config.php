ecd0eb7f0ee2531dbb8f4082b0713f9e6d7e6d4e
alexam99 <alexam99@vt.edu>
2021-04-13 08:56:09 -0400

    Adding and updating db.php and config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  define('DB_HOST', "pamplin-bit2020.mysql.database.azure.com"); 
+  define('DB_USER', "bit4444s21-group01");  
+  define('DB_PASS', "K2WWGm9ALtrxyHyA");
+  define('DB_NAME', "bit4444s21-group01");
+  define("DB_PORT", "3306");
+
+?>
