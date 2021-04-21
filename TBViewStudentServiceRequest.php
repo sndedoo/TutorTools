6f2f8ad76fc2cc83467b22bb466156def7d482c0
Thomas-Beamon <80009636+Thomas-Beamon@users.noreply.github.com>
2021-04-21 01:03:50 -0400

    Updated the Search for service request page

--- TBViewStudentServiceRequest.php
+++ TBViewStudentServiceRequest.php
@@ -1,3 +1,9 @@
+<?php
+$classSelected = "";
+$tutorFirstNameSelected = "";
+$tutorLastNameSelected = "";
+?>
+
 <!doctype html>
 <html>
 
@@ -13,112 +19,114 @@
     <script src="myScripts.js"></script>
     <meta author="Thomas Beamon">
     <meta descriptions="This page allows a student to search for a tutor that teaches the class they are looking for">
+    <style>
+        .orange {
+            color: white;
+            background-color: orange;
+            text-align: center;
+            border: 1px solid;
+            border-color: black;
+            padding: 5px;
+        }
+
+        .lightOrange {
+            background-color: peachpuff;
+            text-align: center;
+            border: 1px solid;
+            border-color: black;
+            padding: 5px;
+        }
+    </style>
 
 </head>
 
 <body>
-    <nav class="navBar">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <nav class="navBar">
 
-        <ul class="nav nav-pills">
-            <li class="pillItem"><img src="tutoring-image1.jpg" id="companyLogo" width="100" height="65" alt="Company Logo"></li>
-            <li class="pillItem"><a href="Project-Homepage.html">Homepage</a></li>
-            <li class="active pillItem"><a href="#">New Service Request</a></li>
-            <li class="pillItem"><a href="Thomas-ViewStudentSchedule.html">View My Schedule</a></li>
-            <li class="pillItem"><a href="Thomas-ViewStudentProfile.html">View My Profile</a></li>
-            <li class="pillItem"><a href="Thomas-EditAndBuildProfile.html">Edit/Build My Profile</a></li>
-        </ul>
-    </nav>
+            <ul class="nav nav-pills">
+                <li class="pillItem"><img src="Image/Tutor Tools Logo.png" id="companyLogo" width="100" height="65" alt="Company Logo"></li>
+                <li class="pillItem"><a href="Project-Homepage.html">Homepage</a></li>
+                <li class="active pillItem"><a href="#">New Service Request</a></li>
+                <li class="pillItem"><a href="Thomas-ViewStudentSchedule.html">View My Schedule</a></li>
+                <li class="pillItem"><a href="Thomas-ViewStudentProfile.html">View My Profile</a></li>
+                <li class="pillItem"><a href="Thomas-EditAndBuildProfile.html">Edit/Build My Profile</a></li>
+            </ul>
+        </nav>
 
-    <!--Student can select what class they are looking for-->
-    <nav class="navBar">
-        <ul class="nav nav-pills nav-fill">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Class 1</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Class 2</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Class 3</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Class 4</a>
-            </li>
-        </ul>
-    </nav>
+        <!--Search bar for a specific class-->
+        <label id="classLabel">Search for class:
+            <input name="classSelected" type="text" size="30" placeholder='e.g. "BIT 4444"' autofocus="" value="<?php echo $classSelected; ?>">
+        </label>
 
-    <div class="tutorInformation">
-        <!--Insert connection to Tutor profile picture here-->
-        <img src="tutoring-image2.jpg" id="profilePicture" width="200" height="200" alt="Student Profile Picture">
-    </div>
+        </br>
 
-    <!--Sign up table for students to sign up for a time slot with tutors-->
-    <div class="signUpTable">
-        <!--Will need to create a loop to assign id values because they will eventually differ in length-->
-        <table>
-            <th colspan="2">
-                Available Tutor Times
-            </th>
-            <tr>
-                <td>
-                    Available Time 1
-                </td>
-                <td>
-                    <button onClick="signUp(this.id)" class="signUpButton" id="1">Sign Up</button>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Available Time 2
-                </td>
-                <td>
-                    <button onClick="signUp(this.id)" class="signUpButton" id="2">Sign Up</button>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Available Time 3
-                </td>
-                <td>
-                    <button onClick="signUp(this.id)" class="signUpButton" id="3">Sign Up</button>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Available Time 4
-                </td>
-                <td>
-                    <button onClick="signUp(this.id)" class="signUpButton" id="4">Sign Up</button>
-                </td>
-            </tr>
-        </table>
-    </div>
+        <!--Search bar for a specific tutor firstname-->
+        <label id="tutorFirstNameLabel">Search for tutor first name:
+            <input name="tutorFirstNameSelected" type="text" size="30" placeholder='e.g. "Cam"' autofocus="" value="<?php echo $tutorFirstNameSelected; ?>">
+        </label>
 
-    <!--Rate for particular class-->
-    <div class="rate">
-        <p>This is where the rate will go</p>
+        </br>
 
-    </div class="basicInformation">
-    <!--View Tutor Name-->
-    <p>Here is where the tutor name will go when we link it to the database</P>
-    <!--Insert Connection to tutor bio-->
-    <p id="tutorBio">Here is where we will link the tutor's bio</p>
-    <div>
-        <ul class="TutorRatings">
-            <li>Overall Rating:
-                <input name="TutorRating" type="range" min="0" max="10" step="1" value="">
-            </li>
-            <li>Attitude Rating:
-                <input name="TutorRating" type="range" min="0" max="10" step="1" value="">
-            </li>
-            <li>Explaination Rating:
-                <input name="TutorRating" type="range" min="0" max="10" step="1" value="">
-            </li>
-            <li>Helpfulness rating
-                <input name="TutorRating" type="range" min="0" max="10" step="1" value="">
-            </li>
-        </ul>
-    </div>
-    <button id="emailTutor"><a href="mailto:tbeamon@vt.edu?subject=Are you available?&body=Hello I was inquiring about a tutoring sesssion at *Specific Time* and am wondering if we could meet.">Here is where the tutor's email will be displayed:</a></button>
-    <div class="Copyright">Copyright of TutorTools &copy;</div>
+        <!--Search bar for a specific tutor Lastname-->
+        <label id="tutorLabel">Search for tutor last name:
+            <input name="tutorLastNameSelected" type="text" size="30" placeholder='e.g. "Combs"' autofocus="" value="<?php echo $tutorLastNameSelected; ?>">
+        </label>
+
+        </br>
+
+        <input type="submit" name="submit" value="Submit" />
+
+
+    </form>
+
+    </br>
+    
+    <?php
+    if (isset($_POST["submit"])) {
+        if (isset($_POST["classSelected"])) $classSelected = $_POST["classSelected"];
+        if (isset($_POST["tutorFirstNameSelected"])) $tutorFirstNameSelected = $_POST["tutorFirstNameSelected"];
+        if (isset($_POST["tutorLastNameSelected"])) $tutorLastNameSelected = $_POST["tutorLastNameSelected"];
+        require_once("db.php");
+
+        $sql = "SELECT Tutor_First, Tutor_Last, Tutor_GradYr, Tutor_Email, Tutor_Class1, Tutor_Class2, Tutor_Class3, Tutor_Class4, Meeting.Meet_Time, Meeting.Meet_Location FROM Tutor JOIN Meeting ON Tutor.Meet_ID = Meeting.Meet_ID WHERE Tutor_Class1= '$classSelected' OR Tutor_Class2= '$classSelected'OR Tutor_Class3= '$classSelected'OR Tutor_Class4= '$classSelected'OR Tutor_First= '$tutorFirstNameSelected'OR Tutor_Last= '$tutorLastNameSelected'";
+        $result = $mydb->query($sql);
+        while ($row = mysqli_fetch_array($result)) {
+        }
+        $result = $mydb->query($sql);
+        if ($classSelected != "" && ($classSelected == $row["Tutor_Class1"] || $classSelected == $row["Tutor_Class2"] || $classSelected == $row["Tutor_Class3"] || $classSelected == $row["Tutor_Class4"] || $classSelected == $row["Tutor_Class4"] || $tutorFirstNameSelected == $row["Tutor_First"] || $tutorLastNameSelected == $row["Tutor_Last"])) {
+            $sql = "SELECT Tutor_First, Tutor_Last, Tutor_GradYr, Tutor_Email FROM Tutor WHERE Tutor_Class1= '$classSelected' OR Tutor_Class2= '$classSelected'OR Tutor_Class3= '$classSelected'OR Tutor_Class4= '$classSelected'OR Tutor_First= '$tutorFirstNameSelected'OR Tutor_Last= '$tutorLastNameSelected'";
+            echo "<table>";
+            echo "<thead> <tr><th class='orange'>First Name</th><th class='orange'>Last Name</th><th class='orange'>Graduation Year</th><th class='orange'>Tutor_Email</th><th class='orange'>Meeting Time</th><th class='orange'>Meeting Location</th><th class='orange'>Class Requested</th><th class='orange'>Sign Up</th></tr>
+                    </thead><tbody>";
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr><td class='lightOrange'>" . $row["Tutor_First"] . "</td><td class='lightOrange'>" . $row["Tutor_Last"] . "</td><td class='lightOrange'>" . $row["Tutor_GradYr"] . "</td><td class= 'lightOrange'>" . $row["Tutor_Email"] . "</td><td class= 'lightOrange'>" . $row["Meet_Time"] . "</td><td class= 'lightOrange'>" . $row["Meet_Location"] . "</td><td class= 'lightOrange'>" . $classSelected . "</td><td class= 'lightOrange'><button>Sign Up</button></td></tr>";
+            }
+            echo "</tbody></table>";
+        } else {
+            $result = $mydb->query($sql);
+            if ($tutorFirstNameSelected != "") {
+                echo "<table>";
+                echo "<thead> <tr><th class='orange'>First Name</th><th class='orange'>Last Name</th><th class='orange'>Graduation Year</th><th class='orange'>Tutor_Email</th><th class='orange'>Class 1</th><th class='orange'>Class 2</th><th class='orange'>Class 3</th><th class='orange'>Class 4</th><th class='orange'>Meeting Time</th><th class='orange'>Meeting Location</th></tr>
+                    </thead><tbody>";
+                while ($row = mysqli_fetch_array($result)) {
+
+                    echo "<tr><td class='lightOrange'>" . $tutorFirstNameSelected . "</td><td class='lightOrange'>" . $row["Tutor_Last"] . "</td><td class='lightOrange'>" . $row["Tutor_GradYr"] . "</td><td class= 'lightOrange'>" . $row["Tutor_Email"] . "</td><td class='lightOrange'>" . $row["Tutor_Class1"] . "</td><td class='lightOrange'>" . $row["Tutor_Class2"] . "</td><td class='lightOrange'>" . $row["Tutor_Class3"] . "</td><td class='lightOrange'>" . $row["Tutor_Class4"] . "</td><td class= 'lightOrange'>" . $row["Meet_Time"] . "</td><td class= 'lightOrange'>" . $row["Meet_Location"] . "</td></tr>";
+                }
+                echo "</tbody></table>";
+            } else {
+                echo "<table>";
+                echo "<thead> <tr><th class='orange'>First Name</th><th class='orange'>Last Name</th><th class='orange'>Graduation Year</th><th class='orange'>Tutor_Email</th><th class='orange'>Class 1</th><th class='orange'>Class 2</th><th class='orange'>Class 3</th><th class='orange'>Class 4</th><th class='orange'>Meeting Time</th><th class='orange'>Meeting Location</th></tr>
+                        </thead><tbody>";
+                while ($row = mysqli_fetch_array($result)) {
+
+                    echo "<tr><td class='lightOrange'>" . $row["Tutor_First"] . "</td><td class='lightOrange'>" . $tutorLastNameSelected . "</td><td class='lightOrange'>" . $row["Tutor_GradYr"] . "</td><td class= 'lightOrange'>" . $row["Tutor_Email"] . "</td><td class='lightOrange'>" . $row["Tutor_Class1"] . "</td><td class='lightOrange'>" . $row["Tutor_Class2"] . "</td><td class='lightOrange'>" . $row["Tutor_Class3"] . "</td><td class='lightOrange'>" . $row["Tutor_Class4"] . "</td><td class= 'lightOrange'>" . $row["Meet_Time"] . "</td><td class= 'lightOrange'>" . $row["Meet_Location"] . "</td></tr>";
+                }
+                echo "</tbody></table>";
+            }
+        }
+    }
+    ?>
 </body>
+
+</html>
