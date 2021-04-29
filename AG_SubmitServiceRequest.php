45453c2dac9e9ef2b25d876c32067d5812ccb3d9
agonzales <agonzales98@vt.edu>
2021-04-29 15:02:39 -0400

    Integrated global css style sheet

--- AG_SubmitServiceRequest.php
+++ AG_SubmitServiceRequest.php
@@ -12,7 +12,7 @@
     <meta author="Alejandro Gonzales">
 
     <!--Module 3 style sheet link-->
-    <link rel="stylesheet" href="Module3.css"/>
+    <link rel="stylesheet" href="Webpages.css"/>
 </head>
 <body>
     <div class="container-fluid">
