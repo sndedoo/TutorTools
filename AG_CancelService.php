b42f1f825231b1b4254586b0858025c1806dc8f8
agonzales98 <81432416+agonzales98@users.noreply.github.com>
2021-05-03 17:24:01 -0400

    Cancel use case (*has errors)

---
+++ AG_CancelService.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html> 
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Service Request Submissiont</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <meta author="Alejandro Gonzales">
+
+    <link rel="stylesheet" href="Webpages.css"/>
+</head>
+<body>
+    <?php include('navStudent.php');?>
+
+    <?php
+    require_once("db.php");
+    require_once("config.php");
+
+    $row=$_GET['id'];
+
+    $query = "DELETE * FROM meeting WHERE Meet_ID = '$row'";
+
+    $data = mysqli_query($conn, $query);
+
+    if($data){
+        echo "<h2>This appointment has been canceled.</h2>";
+    }
+    else{
+        echo "<h2>Failed to cancel appointment, please try again.</h2>";
+    }
+    ?>
+
+
+</body>
+</html> 
