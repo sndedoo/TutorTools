b2897976d59da0d2f30fd57dca90fcc070bffdb8
agonzales98 <81432416+agonzales98@users.noreply.github.com>
2021-05-03 17:32:49 -0400

    Use case 5 page (*Incomplete)

--- AG_ServiceDetails_Analysis.php
+++ AG_ServiceDetails_Analysis.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>UC5 & UC6: View service request details & analysis</title>
+    <title>Service Details & Analytics</title>
     <link href="css/bootstrap.min.css" rel="stylesheet"/>
     <script src="jquery-3.1.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -15,32 +15,7 @@
     <link rel="stylesheet" href="Webpages.css"/>
 </head>
 <body>
-    <div class="container-fluid">
-    
-    <!--Description of this page-->
-    <div>
-        <h1 class="blue"><img src="Image/Tutor Tools Logo.png" alt="Error loading image" align = "left" height="150"/>
-            Use Case 5 & 6: View a service request details & service big data analysis 
-        </h1>
-    </div>    
-    <!--Navigation Bar-->
-    <nav> 
-        <ul class = "nav nav-pills nav-justified">
-            <li><a href="#">Homepage</a></li>
-            <li role = "presentation" class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#"
-                role="button" aria-haspopup="true" aria-haspopup="false">Services<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Search for an Appointment</a></li>
-                    <li><a href="#">Modify an Appointment</a></li>
-                    <li><a href="#">Cancel an Appointment</a></li>
-                </ul>
-            </li>
-            <li><a href = "#">Other Page...</a></li>
-            <li><a href = "#">Other Page...</a></li>
-            <li><a href= "#">Search for Service Requests*</a></li>
-        </ul>
-    </nav>
+    <?php include('navTutor.php');?>
    
 
     <div class="inline">
